1a99b2d408e347fd261a6cfb6e3e98a89b4aff6a
Rich Churcher <rich.churcher@gmail.com>
2016-06-13 20:51:11 +1200

    Basic events and styles

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+  addListeners()
+}
+
+function addListeners () {
+  var cells = document.getElementsByClassName('board')[0].children
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', showCell)
+    cells[i].addEventListener('contextmenu', markCell)
+  }
+}
+
+function showCell (e) {
+  e.target.classList.remove('hidden')
+}
+
+function markCell (e) {
+  e.preventDefault()
+  e.target.classList.add('marked')
 }
