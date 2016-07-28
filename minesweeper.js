fff128965a0676daa138cdfa975e677fa39bf1fb
Rich Churcher <rich.churcher@gmail.com>
2016-07-28 13:53:48 +1200

    Refactoring, concentrate on objects

--- minesweeper.js
+++ minesweeper.js
@@ -5,92 +5,46 @@ var board = {
 }
 
 function startGame () {
-  var elements = document.getElementsByClassName('board')[0].children
-  for (var i = 0; i < elements.length; i++) {
-    addCellToBoard(elements[i])
-    addListeners(elements[i])
+  var ok = lib.initBoard()
+  if (!ok) {
+    return
   }
-  for (var i = 0; i < board.cells.length; i++) {
-    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
-  }
+
+  //var elements = document.getElementsByClassName('board')[0].children
+  //for (var i = 0; i < elements.length; i++) {
+    //addCellToBoard(elements[i])
+    //addListeners(elements[i])
+  //}
+  //for (var i = 0; i < board.cells.length; i++) {
+    //board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  //}
 }
 
 function addCellToBoard (element) {
-  var newCell = {}
-  newCell.row = getRow(element)
-  newCell.col = getCol(element)
-  newCell.isMine = element.classList.contains('mine')
-  board.cells.push(newCell)
-}
-
-function addListeners (element) {
-  element.addEventListener('click', showCell)
-  element.addEventListener('contextmenu', markCell)
-}
-
-function showCell (e) {
-  e.target.classList.remove('hidden')
-  if (e.target.classList.contains('mine')) {
-    return showAllMines()
-  }
-  showSurrounding(e.target)
-  checkForWin()
-}
-
-function markCell (e) {
-  e.preventDefault()
-  e.target.classList.toggle('marked')
-  e.target.classList.remove('hidden')
-  e.target.innerHTML = ''
-  board.cells
-    .find(function (cell) {
-      return cell.row === getRow(e.target) && cell.col === getCol(e.target)
-    })
-    .isMarked = true
-  checkForWin()
+  //var newCell = {}
+  //newCell.row = getRow(element)
+  //newCell.col = getCol(element)
+  //newCell.isMine = element.classList.contains('mine')
+  //board.cells.push(newCell)
 }
 
 function checkForWin () {
-  if (board.cells.filter(function (cell) {
-    return cell.isMine && !cell.isMarked
-  }).length > 0) {
-    return
-  }
-  var cells = document.getElementsByClassName('board')[0]
-  if (cells.getElementsByClassName('hidden').length > 0) {
-    return
-  }
-  alert('WIN')
-}
-
-function showAllMines () {
-  var mines = document.getElementsByClassName('mine')
-  for (var i = 0; i < mines.length; i++) {
-    mines[i].classList.remove('hidden')
-    mines[i].classList.remove('marked')
-  }
+  //if (board.cells.filter(function (cell) {
+    //return cell.isMine && !cell.isMarked
+  //}).length > 0) {
+    //return
+  //}
+  //var cells = document.getElementsByClassName('board')[0]
+  //if (cells.getElementsByClassName('hidden').length > 0) {
+    //return
+  //}
+  //alert('WIN')
 }
 
 function countSurroundingMines (cell) {
-  return getSurroundingCells(cell.row, cell.col)
-    .filter(function (adjacentCell) {
-      return adjacentCell.isMine
-    }).length;
+  //return getSurroundingCells(cell.row, cell.col)
+    //.filter(function (adjacentCell) {
+      //return adjacentCell.isMine
+    //}).length;
 }
 
-function getRow (element) {
-  return getLoc(element, 'row')
-}
-
-function getCol (element) {
-  return getLoc(element, 'col')
-}
-
-function getLoc(element, coordinate) {
-  for (var i = 0; i < element.classList.length; i++) {
-    if (element.classList[i].substring(0, 3) === coordinate) {
-      return parseInt(element.classList[i].split('-')[1], 10)
-    }
-  }
-  return null
-}
--- minesweeper_lib.js
+++ minesweeper_lib.js
@@ -1,70 +1,34 @@
-// Expects a `board` object in global scope
-// Returns a subset of the `cells` array, including only those cells
-// which are adjacent to `row`, `col`
-function getSurroundingCells (row, col) {
-  var columns = getRange(getLowerBound(col), getUpperBound(col))
-  var rows = getRange(getLowerBound(row), getUpperBound(row))
-  return board.cells
-    .filter(function (cell) {
-      return columns.includes(cell.col) && rows.includes(cell.row)
-    })
+var lib = {
+  initBoard: initBoard
 }
 
-// For the given DOM element, displays surrounding mine counts
-// under the following conditions:
-//  - cell is not a mine
-//  - cell has not already been checked
-function showSurrounding (element) {
-  getSurroundingCells(getRow(element), getCol(element))
-    .filter(function (cell) {
-      return !cell.isMine
-    })
-    .filter(function (cell) {
-      // Avoid breaking the call stack with recurrent checks on same cell
-      return !cell.isProcessed
-    })
-    .forEach(setInnerHTML)
-}
-
-// For the given cell object, set innerHTML to cell.surroundingMines
-// under the following conditions:
-//  - cell has not been marked by the user
-//  - surroundingMines is > 0
-// If surroundingMines is 0, greedily attempt to expose as many more cells
-// as possible.
-function setInnerHTML (cell) {
-  cell.isProcessed = true
-  var rowClass = 'row-' + cell.row
-  var colClass = 'col-' + cell.col
-  var element = document.getElementsByClassName(rowClass + ' ' + colClass)[0]
-  if (element.innerHTML !== '') {
-    return
+function initBoard () {
+  if (!isValid()) {
+    return null
   }
-  if (element.classList.contains('marked')) {
-    return
+  displayMessage('Let\'s play!')
+}
+
+function isValid() {
+  if (typeof board !== 'object') {
+    displayMessage('Remember to define your <code>board</code> object!')
+    return false
   }
-  element.innerHTML = cell.surroundingMines > 0 ? 
-    cell.surroundingMines : ''
-  if (element.classList.contains('hidden')) {
-    element.classList.remove('hidden')
-    if (cell.surroundingMines === 0) {
-      return showSurrounding(element)
-    }
+  if (!board.hasOwnProperty('cells')) {
+    displayMessage('Your <code>board</code> object needs a property named <code>cells</code>.')
+    return false
   }
+  if (!isArray(board.cells)) {
+    displayMessage('<code>board.cells</code> should be an array.')
+    return false
+  }
+  return true
 }
 
-function getRange(begin, end) {
-  return Array.apply(begin, Array(end - begin + 1))
-    .map(function (n, i) {
-      return begin + i
-    })
+function displayMessage (msg) {
+  document.getElementById('message').innerHTML = '<p>' + msg + '</p>'
 }
 
-function getLowerBound (n) {
-  return n - 1 < 0 ? 0 : n -1
-}
-
-function getUpperBound (n) {
-  var limit = board.MAX_CELLS-1 || 4
-  return n + 1 > limit ? limit : n + 1
+function isArray(a) {
+  return Object.prototype.toString.call(a) === '[object Array]' 
 }
