bd1c8b36ab7741cf7b4fb04b462e2392ba678521
Rich Churcher <rich.churcher@gmail.com>
2016-07-29 15:43:31 +1200

    Count surrounding

--- minesweeper.js
+++ minesweeper.js
@@ -4,53 +4,26 @@ document.addEventListener('DOMContentLoaded', startGame)
 var board = {
   cells: [
     { row: 0, col: 0, isMine: false, hidden: false }, 
-    { row: 0, col: 1, isMine: false, hidden: false, surroundingMines: 1 }, 
+    { row: 0, col: 1, isMine: false, hidden: false }, 
     { row: 0, col: 2, isMine: true, hidden: true }, 
-    { row: 0, col: 3, isMine: true, hidden: false }, 
-    { row: 0, col: 4, isMine: true, hidden: false }, 
-    { row: 0, col: 5, isMine: true, hidden: false }, 
     { row: 1, col: 0, isMine: true, hidden: true }, 
     { row: 1, col: 1, isMine: false, hidden: true }, 
-    { row: 1, col: 2, isMine: true, hidden: true }, 
-    { row: 1, col: 3, isMine: false, hidden: true }, 
-    { row: 1, col: 4, isMine: true, hidden: true }, 
-    { row: 1, col: 5, isMine: false, hidden: true }, 
-    { row: 2, col: 0, isMine: true, hidden: true }, 
-    { row: 2, col: 1, isMine: true, hidden: true }, 
-    { row: 2, col: 2, isMine: false, hidden: true }, 
-    { row: 2, col: 3, isMine: true, hidden: true }, 
-    { row: 2, col: 4, isMine: true, hidden: true }, 
-    { row: 2, col: 5, isMine: false, hidden: true }, 
-    { row: 3, col: 0, isMine: true, hidden: true },
-    { row: 3, col: 1, isMine: true, hidden: false }, 
-    { row: 3, col: 2, isMine: false, hidden: true }, 
-    { row: 3, col: 3, isMine: true, hidden: true }, 
-    { row: 3, col: 4, isMine: true, hidden: false }, 
-    { row: 3, col: 5, isMine: true, hidden: false }, 
-    { row: 4, col: 0, isMine: true, hidden: false }, 
-    { row: 4, col: 1, isMine: false, hidden: true }, 
-    { row: 4, col: 2, isMine: true, hidden: true }, 
-    { row: 4, col: 3, isMine: true, hidden: false }, 
-    { row: 4, col: 4, isMine: true, hidden: false }, 
-    { row: 4, col: 5, isMine: true, hidden: false }, 
-    { row: 5, col: 0, isMine: true, hidden: false }, 
-    { row: 5, col: 1, isMine: false, hidden: true }, 
-    { row: 5, col: 2, isMine: true, hidden: true }, 
-    { row: 5, col: 3, isMine: true, hidden: false }, 
-    { row: 5, col: 4, isMine: true, hidden: false }, 
-    { row: 5, col: 5, isMine: true, hidden: false } 
+    { row: 1, col: 2, isMine: false, hidden: true }, 
+    { row: 2, col: 0, isMine: false, hidden: true }, 
+    { row: 2, col: 1, isMine: false, hidden: true }, 
+    { row: 2, col: 2, isMine: false, hidden: true } 
   ]
 }
 
 function startGame () {
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  }
+
   // Don't remove this if statement: it makes the game work!
   if (!lib.refresh()) {
     return
   }
-
-  //for (var i = 0; i < board.cells.length; i++) {
-    //board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
-  //}
 }
 
 function checkForWin () {
@@ -68,10 +41,8 @@ function checkForWin () {
 
 function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-
-  //return getSurroundingCells(cell.row, cell.col)
-    //.filter(function (adjacentCell) {
-      //return adjacentCell.isMine
-    //}).length;
+  return surrounding.filter(function (adjacentCell) {
+    return adjacentCell.isMine
+  }).length;
 }
 
