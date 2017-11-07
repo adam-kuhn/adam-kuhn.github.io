974f677115903615cc550c35bfddad763b3c792f
adam-kuhn <adam.kuhn20@gmail.com>
2017-11-08 10:24:25 +1300

    add event listener to chekForwin

--- minesweeper.js
+++ minesweeper.js
@@ -8,10 +8,10 @@ var board = {
     {row:0, col:2, isMine: true, hidden: true},
     {row:1, col:0, isMine: true, hidden: true},
     {row:1, col:1, isMine: false, hidden: true},
-    {row:1, col:2, isMine: true, hidden: true},
+    {row:1, col:2, isMine: false, hidden: true},
     {row:2, col:0, isMine: true, hidden: true},
     {row:2, col:1, isMine: true, hidden: true},
-    {row:2, col:2, isMine: true, hidden: true},
+    {row:2, col:2, isMine: false, hidden: true},
   ]
 
 
@@ -23,6 +23,8 @@ function startGame () {
   for (cell in board.cells){
     board.cells[cell].surroundingMines = countSurroundingMines(cell);
   }
+
+  document.addEventListener('dblclick', checkForWin
 
 
   // Don't remove this function call: it makes the game work!
