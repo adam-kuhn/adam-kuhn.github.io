79f4a163b0adeb0db8ceef78d4080b8e51554e31
adam-kuhn <adam.kuhn20@gmail.com>
2017-11-10 11:17:55 +1300

    completed win condition generic answer though. Will have to change this code if the number of mines change

--- minesweeper.js
+++ minesweeper.js
@@ -37,28 +37,70 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-  let checkCell = 0;
+// define win first?
+/*let checkMine = 0
+for (checkMine in board.cells){
+if (board.cells[checkMine].isMine === true && board.cells[checkMine].isMarked === true || board.cells[checkMine].isMine === false && board.cells[checkMine].hidden === false){
+  //code to make game continue - loop for continue
+  let checkCell = 0
   for (checkCell in board.cells){
-    if (board.cells[checkCell].isMine === false && board.cells[checkCell].hidden === true){
-
+    if (board.cells[checkCell].isMine === true && board.cells[checkCell].isMarked === false || board.cells[checkCell].isMine === false && board.cells[checkCell].hidden === true ){
+      return
+    } else if (board.cells[checkCell].isMine === false && board.cells[checkCell].hidden === true){
+      return
+    } else if (board.cells[checkCell].isMine === false && board.cells[checkCell].hidden === false){
+      return lib.displayMessage('ahha');
     }
-
-    if (board.cells[checkCell].isMine === true && board.cells[checkCell].isMarked === false){
-
-    }
-
-    if (board.cells[checkCell].isMine === true && board.cells[checkCell].isMarked === true){
-      let checkHidden = 0
-      for (checkHidden in board.cells){
-      if (board.cells[checkHidden].hidden === false && board.cells[checkHidden].isMine === false){
-
-      return lib.displayMessage('You win!');
+    //} else if (board.cells[checkCell].isMine === false && board.cells[checkCell].hidden === false){
+      //return lib.displayMessage('You Win!')
     }
   }
+}*/
+
+
+  //only requires 1 mine checked and 1 non-mine checked to trigger win...
+  let nonMine = 0;
+  let markedMine = 0;
+
+  let checkCell = 0;
+  for (checkCell in board.cells){
+    if (board.cells[checkCell].hidden === true && board.cells[checkCell].isMine === false){
+       nonMine += 0;
+
+    } else if (board.cells[checkCell].isMine === true && board.cells[checkCell].isMarked === false){
+        markedMine += 0;
+    } else if (board.cells[checkCell].hidden === false && board.cells[checkCell].isMine === false){
+        nonMine += 1;
+    } else if (board.cells[checkCell].isMine === true && board.cells[checkCell].isMarked === true){
+       markedMine += 1;
+    }
 }
+if (nonMine == 3 && markedMine == 6){
+return lib.displayMessage('You Win!')
+}
+
+
+    /*if (board.cells[checkCell].hidden === false && board.cells[checkCell].isMine === false){
+      let checkMarked = 0
+      for (checkMarked in board.cells){
+        if (board.cells[checkMarked].isMine === true && board.cells[checkMarked].isMarked === false){
+
+        } else if (board.cells[checkMarked].isMine === true && board.cells[checkMarked].isMarked === true){
+          let checkAgain = 0
+          for (checkAgain in board.cells){
+            if (board.cells[checkAgain].isMine === true && board.cells[checkAgain].isMarked === false){
+
+            } else if (board.cells[checkAgain].isMine === false && board.cells[checkAgain].hidden === true){
+
+            } else if (board.cells[checkAgain].isMine === true && board.cells[checkAgain].hidden === false){
+            return lib.displayMessage('You Win!')
+            }
+          }
         }
 
 
+      }
+    }*/
 
 
 
@@ -82,14 +124,14 @@ function checkForWin () {
 
 
 function countSurroundingMines (cell) {
-//let adjacentCell = undefined;
+
   var surroundingCells = lib.getSurroundingCells(board.cells[cell].row, board.cells[cell].col);
 
 let count = 0;
 let selectedCell = 0;
 
   for (selectedCell in surroundingCells) {
-    if (surroundingCells[selectedCell].isMine === true) {
+    if (surroundingCells[selectedCell].isMine == true) {
       count += 1;
     }
   }
