8673b1f7f2e10e8b41837e72cf1b4d7f981b2774
adam-kuhn <adam.kuhn20@gmail.com>
2017-11-17 12:00:16 +1300

    working maori proverbs

--- blog/p2-translate-JS.js
+++ blog/p2-translate-JS.js
@@ -1,31 +1,11 @@
 
 window.onload = function(){
 document.getElementById("ajaxButton").addEventListener('click', makeRequest);
+makeRequest();
 }
 
-/*
-.ajax({
-   url: 'https://eda-te-reo.herokuapp.com/api/whakatauki',
-   data: {
-      format: 'json'
-   },
-   error: function() {
-      $('#info').html('<p>An error has occurred</p>');
-   },
-   dataType: 'jsonp',
-   success: function(data) {
-      var $title = $('<h1>').text(data.talks[0].talk_title);
-      var $description = $('<p>').text(data.talks[0].talk_description);
-      $('#info')
-         .append($title)
-         .append($description);
-   },
-   type: 'GET'
-});
-*/
 
 
-// Request Works - but not JSON
 function makeRequest() {
   var httpRequest = new XMLHttpRequest();
   if (!httpRequest){
@@ -40,45 +20,15 @@ function makeRequest() {
   function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE){
       if (httpRequest.status === 200) {
-        var quotes = document.getElementById("quotes");
-        quotes.innerHTML = httpRequest.responseText;
-        //quotes(httpRequest.responseText);
-        //document.getElementById("quotes").println(httpRequest.response.Text);
-        //alert(httpRequest.responseText);
+
+        var quotes = JSON.parse(this.responseText);
+        //split source and translation
+        document.getElementById("maori").innerHTML = quotes.source;
+        document.getElementById("english").innerHTML = quotes.translation;
+
       } else {
         alert('There was a problem with the request.');
       }
     }
   }
 }
-
-
-
-
-
-/*function() {
-  var httpRequest;
-  document.getElementById("ajaxButton").addEventListener('click', makeRequest);
-
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
-      return false;
-    }
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', 'https://eda-te-reo.herokuapp.com/api/whakatauki', true);
-    httpRequest.send();
-  }
-
-  function alertContents() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        alert(httpRequest.responseText);
-      } else {
-        alert('There was a problem with the request.');
-      }
-    }
-  }
-}*/
